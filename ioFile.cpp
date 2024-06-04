1171c61352716b9d1bba2d6ea725905c47ac5fc0
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-06-04 10:14:03 +0700

    menyimpan dan membuka sebuah file

--- ioFile.cpp
+++ ioFile.cpp
@@ -1,4 +1,18 @@
 #include <fstream>
 #include <iostream>
 #include  <string>
-using namespace; 
+using namespace std; 
+
+int main() {
+	
+	//untuk menyimpan data berbentuk string
+	string baris; 
+
+	//untuk membuka file dalam mode menulis. 
+	ofstream outfile;
+	// menunjuk ke sebuah nama file 
+	outfile.open("contohfile.txt");
+
+	cout << ">= Membuka file, \'q\' untuk keluar" << endl; 
+
+}
